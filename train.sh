92f9cd29272e3dab0501fcdd3a0506cd465e7cbb
xlmxbw <xlmxbw@aliyun.com>
2023-01-04 11:39:14 +0800

    change yolov5.7

---
+++ train.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# -*- coding:utf-8 -*-
+
+model='.lite'
+module='07'
+epochs=1000
+img_size=640
+batch_size=64
+label_smoothing=0.0
+
+
+python3 yolov5.7$model/train.py \
+    --weights '' \
+    --cfg yaml/model.$module.yaml \
+    --data yaml/data.traffic_light.yaml \
+    --hyp yaml/hyp.traffic_light.yaml \
+    --label-smoothing $label_smoothing \
+    --batch-size $batch_size \
+    --img-size $img_size \
+    --optimizer 'AdamW' \
+    --project run/train \
+    --epochs $epochs \
+    --seed $epochs \
+    --device 0 \
+    --name traffic_light_model$model.$module
